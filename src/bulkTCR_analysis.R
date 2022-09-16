f4f8dd4837a282fed79ef715eb2999b2e85b8789
tan <ziyang.tan@ki.se>
2022-09-16 16:31:18 +0200

    add comparison to 2 public db and bug fix

--- src/bulkTCR_analysis.R
+++ src/bulkTCR_analysis.R
@@ -5,6 +5,10 @@ library(ggpubr)
 
 raw <- read_csv('data/bulk_data.csv.gz')
 raw_ctrl <- read_csv('data/bulk_data_ctrl.csv.gz')
+raw_ISAC <- read_csv('data/bulk_data_ISAC_ref.csv.gz') %>% 
+  rename(`NGI ID` = ngi_id) %>% 
+  left_join(read_csv('data/bulk_info_ISAC_ref.csv.gz'), by = 'NGI ID')
+public_ref <- readxl::read_excel('data/public kids control.xlsx')
 
 process_mixcr_raw <- function(raw){
   raw <- raw[,colSums(!is.na(raw))!=0]
@@ -28,11 +32,12 @@ count_V_fraction <- function(data) {
       summarise(freq = sum(cloneFraction))
     colnames(tmp) <- c('V', cur_sample)
     return(tmp)
-  }) %>% Reduce(f=function(x,y) left_join(x,y,by='V'))
+  }) %>% Reduce(f=function(x,y) full_join(x,y,by='V'))
 }
 
 res <- process_mixcr_raw(raw) %>% count_V_fraction()
 res_ctrl <- process_mixcr_raw(raw_ctrl) %>% count_V_fraction()
+res_ISAC <- process_mixcr_raw(raw_ISAC) %>% count_V_fraction()
 
 rowOrder <- (res %>% rowwise() %>% mutate(mean = mean(c_across(contains('sample')), na.rm = T)) %>% arrange(desc(mean)))$V
 
@@ -44,10 +49,22 @@ res.long <- res %>%
 res_ctrl.long <- res_ctrl %>%
   mutate(V = factor(V, levels = rowOrder)) %>%
   tidyr::pivot_longer(cols = setdiff(colnames(res_ctrl), c('V')), names_to = 'subject', values_to = 'freq') %>%
-  mutate(group = if_else(subject == 'PBMC Ctrl', 'PBMC control', 'ISAC34'))
+  mutate(group = if_else(subject == 'PBMC Ctrl', 'PBMC control', 'ISAC'))
 
-ggplot(rbind(res.long, res_ctrl.long) %>% filter(group != 'ISAC34'), aes(x=V, y=freq, color=group)) +
-  geom_boxplot() + 
+res_ISAC.long <- res_ISAC %>%
+  mutate(V = factor(V, levels = rowOrder)) %>%
+  tidyr::pivot_longer(cols = setdiff(colnames(res_ISAC), c('V')), names_to = 'subject', values_to = 'freq') %>%
+  mutate(group = 'ISAC') %>%
+  filter(freq<0.5)
+
+res_public.long <- public_ref %>%
+  mutate(V = factor(V, levels = rowOrder)) %>%
+  tidyr::pivot_longer(cols = setdiff(colnames(public_ref), c('V')), names_to = 'subject', values_to = 'freq') %>%
+  mutate(group = 'public children control')
+
+ggplot(rbind(res.long, res_ctrl.long, res_ISAC.long, res_public.long), aes(x=V, y=freq, color=group)) +
+  geom_boxplot(outlier.shape = NA) + 
+  geom_jitter(aes(group=group), position=position_jitterdodge(), size=0.3, alpha=0.5) + 
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 
-ggsave(filename = 'figures/V_distribution.pdf')
+ggsave(filename = 'figures/V_distribution_with_ISAC_ref.pdf', width = 15, height = 8)
